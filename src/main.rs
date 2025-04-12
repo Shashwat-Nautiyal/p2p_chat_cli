79984a4cb184a4cd1856d50904ce0e9c32e735f8
Shashwat-Nautiyal <shash01nautiyal@gmail.com>
2025-04-13 02:10:57 +0530

    prototype is ready

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
+use iroh::NodeAddr;
+use std::fmt;
+use std::str::FromStr;
 use std::collections::HashMap;
+use clap::Parser;
 use anyhow::Result;
 use iroh::protocol::Router;
 use iroh::Endpoint;
@@ -8,8 +12,52 @@ use iroh::NodeId;
 use serde::{Deserialize, Serialize};
 use futures_lite::StreamExt;
 
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Set your nickname.
+    #[clap(short, long)]
+    name: Option<String>,
+    /// Set the bind port for our socket. By default, a random port will be used.
+    #[clap(short, long, default_value = "0")]
+    bind_port: u16,
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Parser, Debug)]
+enum Command {
+    /// Open a chat room for a topic and print a ticket for others to join.
+    Open,
+    /// Join a chat room from a ticket.
+    Join {
+        /// The ticket, as base32 string.
+        ticket: String,
+    },
+}
+
+
 #[tokio::main]
 async fn main() -> Result<()> {
+
+    let args = Args::parse();
+
+    // parse the cli command
+    let (topic, nodes) = match &args.command {
+        Command::Open => {
+            let topic = TopicId::from_bytes(rand::random());
+            println!("> opening chat room for topic {topic}");
+            (topic, vec![])
+        }
+        Command::Join { ticket } => {
+            let Ticket { topic, nodes } = Ticket::from_str(ticket)?;
+            println!("> joining chat room for topic {topic}");
+            (topic, nodes)
+        }
+    };
+
+    //It crates a netwrok interface for our node
+    //endpoint will bind to an IP address and port 
     let endpoint = Endpoint::builder().discovery_n0().bind().await?;
 
     println!("> our node id: {}", endpoint.node_id());
@@ -20,28 +68,53 @@ async fn main() -> Result<()> {
         .spawn()
         .await?;
 
+    
+    let ticket = {
+            let me = endpoint.node_addr().await?;
+            let nodes = vec![me];
+            Ticket { topic, nodes }
+    };
+    println!("> ticket to join us: {ticket}");
+
     // Create a new topic.
-    let id = TopicId::from_bytes(rand::random());
-    let node_ids = vec![];
+    // What is a Topic?
+    // A topic in a gossip protocol is a named channel or category for messages. It acts as a way to:
+    // #Organize communication: Nodes can subscribe to specific topics to send/receive messages relevant to their interests.
+    // #Filter messages: Nodes only process messages for topics they care about, reducing unnecessary network traffic.
 
     // Subscribe to the topic.
     // Since the `node_ids` list is empty, we will
     // subscribe to the topic, but not attempt to
     // connect to any other nodes.
-    let topic = gossip.subscribe(id, node_ids)?;
+
+    
 
     // `split` splits the topic into the `GossipSender`
     // and `GossipReceiver` portions
-    let (sender, _receiver) = topic.split();
-
-
-    let message = Message::AboutMe {
-        from: endpoint.node_id(),
-        name: String::from("shash"),
+    let node_ids = nodes.iter().map(|p| p.node_id).collect();
+    if nodes.is_empty() {
+        println!("> waiting for nodes to join us...");
+    } else {
+        println!("> trying to connect to {} nodes...", nodes.len());
+        // add the peer addrs from the ticket to our endpoint's addressbook so that they can be dialed
+        for node in nodes.into_iter() {
+            endpoint.add_node_addr(node)?;
+        }
     };
-    // Turn the message into a `Vec`, and then use
-    // `into` to coerse the `Vec` into `Bytes`
-    sender.broadcast(message.to_vec().into()).await?;
+
+
+    let (sender, _receiver) = gossip.subscribe_and_join(topic, node_ids).await?.split();
+    println!("> connected!");
+
+    // broadcast our name, if set
+    if let Some(name) = args.name {
+        let message = Message::AboutMe {
+            from: endpoint.node_id(),
+            name,
+        };
+        sender.broadcast(message.to_vec().into()).await?;
+    }
+
 
     // subscribe and print loop
     tokio::spawn(subscribe_loop(_receiver));
@@ -132,5 +205,40 @@ fn input_loop(line_tx: tokio::sync::mpsc::Sender<String>) -> Result<()> {
         line_tx.blocking_send(buffer.clone())?;
         // clear the buffer after we've sent the content
         buffer.clear();
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Ticket {
+    topic: TopicId,
+    nodes: Vec<NodeAddr>,
+}
+
+impl Ticket {
+    fn from_bytes(bytes: &[u8]) -> Result<Self>{
+        serde_json::from_slice(bytes).map_err(Into::into)
+
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8>{
+        serde_json::to_vec(self).expect("serde_json::to_vec is infallible")
+    }
+}
+
+// The `Display` trait allows us to use the `to_string`
+// method on `Ticket`.
+impl fmt::Display for Ticket {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut text = data_encoding::BASE32_NOPAD.encode(&self.to_bytes()[..]);
+        text.make_ascii_lowercase();
+        write!(f, "{}", text)
+    }
+}
+
+impl FromStr for Ticket {
+    type Err = anyhow::Error;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let bytes = data_encoding::BASE32_NOPAD.decode(s.to_ascii_uppercase().as_bytes())?;
+        Self::from_bytes(&bytes)
     }
 }
